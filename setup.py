424582107eaa6e7f5e09de757a13afb1174c3474
jonas <jonas.bouaziz@epita.fr>
2020-07-17 11:50:10 +0200

    Create custom concepts endpoint

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='PyCrowlingo',
-    version='0.4.2',
+    version='0.4.3',
     packages=find_packages(),
     install_requires=requirements,
     long_description=long_description,
