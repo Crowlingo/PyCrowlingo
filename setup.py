e990c0f119d945539c828d2319ce9f27e0945e8e
jonas <jonas.bouaziz@epita.fr>
2020-11-04 19:21:01 +0100

    fix json decode error

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='PyCrowlingo',
-    version='0.4.9',
+    version='0.5.0',
     packages=find_packages(),
     install_requires=requirements,
     long_description=long_description,
