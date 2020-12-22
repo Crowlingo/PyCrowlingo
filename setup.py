97db61cfc93c9ac9d942c74cdfbc7d64793b5b2a
jonas <jonas.bouaziz@epita.fr>
2020-12-22 22:32:33 +0100

    change requests version requirements.txt for dss compatibility

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='PyCrowlingo',
-    version='0.5.0',
+    version='0.5.1',
     packages=find_packages(),
     install_requires=requirements,
     long_description=long_description,
